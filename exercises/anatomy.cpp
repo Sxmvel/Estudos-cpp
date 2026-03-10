19dd897b20e71256a0c4bea4fc2d4df14ed8d7fb
Sxmvel <samuelres444@gmail.com>
2026-03-10 10:13:15 -0300

    Modos de print C++
    * cout <<
    * printf
    * puts

---
+++ exercises/anatomy.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+
+int main(){
+
+    std:: cout 
+    << "Ola!"
+    <<
+    " Tchau"
+    <<
+    std::endl;
+
+    std ::cout << " End";
+    return 0;
+}
---
+++ exercises/hello2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <cstdio>
+
+
+int main(){
+
+    std ::cout << "Hello C++ #1.0" << std::endl;
+    printf("Hello C++ #2.0\n");
+    puts("Hello C++ #3.0");
+    return 0;
+    
+}
