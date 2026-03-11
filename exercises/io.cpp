e4ac991e6e339dd4eac518435d4f226d6e6c2951
Sxmvel <samuelres444@gmail.com>
2026-03-11 12:43:20 -0300

    Operadores de inserção e extração
    * cin - extraction
    * cout - insertion

---
+++ exercises/io.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Insertion operator 
+
+    cout << "Hi!\n";
+    cout << 3.14 << endl;
+
+    int x = 3;
+
+    cout << x << endl;
+
+    
+    cout << "Enter yout name: ";
+    
+
+    // extraction operator - Operador de extração
+    string name;
+    cin >> name; // Padrão de entrada cpp 
+
+
+    // Insertion Operador - Operador de inserção.
+    cout << name;
+
+
+    return 0;
+}
